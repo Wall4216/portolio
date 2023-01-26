5766d94ad96def024283528f038f7984c0db8b1d
Wall <ilnazamirhanov61@gmail.com>
2023-01-27 01:37:49 +0300

    correction logout system

--- portfolio/resources/js/router/index.js
+++ portfolio/resources/js/router/index.js
@@ -5,19 +5,40 @@ import homeAdminIndex from '../components/admin/home/index.vue'
 import homePageIndex from '../components/pages/home/index.vue'
 
 import notFound from "../components/notFound.vue";
+import login from "../components/auth/login.vue";
 const routes  = [
     {
         path: '/admin/home',
+        name: 'adminHome',
         component: homeAdminIndex,
+        meta: {
+            requires: true
+        }
     },
     {
         path: '/',
+        name: 'Hone',
         component: homePageIndex,
+        meta: {
+            requires: false
+        }
     },
+    {
+        path: '/login',
+        name: 'Login',
+        component: login,
+        meta: {
+            requires: false
+        }
 
+    },
     {
         path: '/:pathMath(.*)*',
         component: notFound,
+        name: notFound,
+        meta: {
+            requires: true
+        }
     }
 ]
 
@@ -26,5 +47,13 @@ const router = createRouter({
     routes,
 })
 
-
+router.beforeEach((to, from) =>{
+    if (to.meta.requiresAuth && !localStorage.getItem('token')){
+        return {name: ''}
+    }
+    if(to.meta.requiresAuth == false && localStorage.getItem('token'))
+    {
+        return {name: 'adminHome'}
+    }
+})
 export default router
