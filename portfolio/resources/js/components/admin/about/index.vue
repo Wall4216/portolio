95325b4f1980f1081e87b5f4a429dd179d2f61cb
Wall <ilnazamirhanov61@gmail.com>
2023-01-27 23:47:42 +0300

    add upload image

---
+++ portfolio/resources/js/components/admin/about/index.vue
@@ -0,0 +1,174 @@
+<script setup>
+import Base from "../layouts/base.vue";
+import {onMounted, reactive} from "vue";
+
+let form = reactive({
+    name: '',
+    photo: '',
+    email: '',
+    phone: '',
+    address: '',
+    description: '',
+    tagline: '',
+    cv: '',
+})
+
+onMounted(async() =>{
+    getAbout()
+})
+
+    const getAbout = async() => {
+        let response = await axios.get("/api/edit_about")
+        form.value = response.data
+        console.log('response', response)
+    }
+
+    const getPhoto = () => {
+        let photo = "/img/logo.png"
+        if(form.photo) {
+            if (form.photo.indexOf('base64') !== -1) {
+                photo = form.photo
+            } else {
+                photo = 'img/upload' + form.photo
+            }
+        }
+        return photo
+    }
+
+    const changePhoto = (e) => {
+    let file = e.target.files[0]
+        let reader = new FileReader();
+    let limit = 1024*1024*2;
+    if (file['size'] > limit) {
+            swal({
+                icon: 'error',
+                title: 'Oooops',
+                text: 'Ты загружаешь большой файл'
+            })
+        return false
+        }
+    reader.onloadend = (file) => {
+        form.photo = reader.result
+    }
+    reader.readAsDataURL(file)
+
+    }
+</script>
+
+<template>
+    <Base />
+    <h1>About</h1>
+    <main class="main">
+        <div class="main__sideNav"></div>
+        <div class="main__content">
+            <section class="about section" id="about">
+                <div class="about_container">
+                    <div class="titlebar">
+                        <div class="titlebar_item">
+                            <h1>About Us</h1>
+                        </div>
+                        <div class="titlebar_item">
+                            <div class="btn btn-secondary">
+                                Save Changes
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card_wrapper">
+                        <div class="wrapper_left">
+                            <div class="card">
+                                <p>Full Name</p>
+                                <input type="text" class="input" v-model="form.name" />
+
+                                <p>Email</p>
+                                <input type="email" class="input" v-model="form.email"/>
+
+                                <p>Phone</p>
+                                <input type="text" class="input" v-model="form.phone"/>
+
+                                <p>Address</p>
+                                <input type="text" class="input" v-model="form.address"/>
+
+                                <p>Description</p>
+                                <textarea cols="10" rows="5"  v-model="form.description"></textarea>
+
+                            </div>
+                            <div class="card">
+                                <p>Tagline</p>
+                                <input type="text" class="input" v-model="form.tagline"/>
+                            </div>
+                        </div>
+
+                        <div class="wrapper_right ">
+                            <div class="card">
+                                <div class="avatar_profile">
+                                    <img :src="getPhoto" alt="" class="avatar_profile_img">
+                                </div>
+                                <input type="file" id="fileimg" @change="changePhoto"/>
+                            </div>
+                            <div class="card">
+                                <p>CV</p>
+                                <input type="file" id="filecv" />
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="titlebar">
+                        <div class="titlebar_item">
+
+                        </div>
+                        <div class="titlebar_item">
+                            <div class="btn btn-secondary">
+                                Save Changes
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!--==================== SKILLS ====================-->
+            <section class="skills section" id="skills">
+
+            </section>
+
+            <!--==================== QUALIFICATION ====================-->
+            <section class="qualification section">
+
+            </section>
+
+            <!--==================== SERVICES ====================-->
+            <section class="services section" id="services">
+
+            </section>
+
+            <!--==================== PORTFOLIO ====================-->
+            <section class="portfolio section" id="portfolio">
+
+            </section>
+
+            <!--==================== PROJECT IN MIND ====================-->
+            <section class="project section">
+
+            </section>
+
+            <!--==================== TESTIMONIAL ====================-->
+            <section class="testimonial section">
+
+            </section>
+
+            <!--==================== CONTACT ME ====================-->
+            <section class="contact section" id="contact">
+
+            </section>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
